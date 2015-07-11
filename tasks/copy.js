dc67e372f7f348037e9ba94fafda61085c057687
Michiel Sikma <michiel@sikma.org>
2015-07-11 13:28:59 +0900

    Copy the public root files properly

--- tasks/copy.js
+++ tasks/copy.js
@@ -1,13 +1,25 @@
 var path = require('path');
 
 module.exports = {
-  'assets': {
+  // Files that end up in the /public/ root.
+  'root': {
     'files': [
       {
         'expand': true,
-        'cwd': path.join(process.cwd(), 'app/assets'),
+        'cwd': path.join(process.cwd(), 'app/assets/root'),
         'src': '**/*',
         'dest': path.resolve('public/')
+      }
+    ]
+  },
+  // Image content (except sprites)
+  'images': {
+    'files': [
+      {
+        'expand': true,
+        'cwd': path.join(process.cwd(), 'app/assets/images'),
+        'src': '**/*',
+        'dest': path.resolve('public/images/')
       }
     ]
   }
