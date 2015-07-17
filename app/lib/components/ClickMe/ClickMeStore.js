422b4f4785cea416bb53273d3aca55f0959ab1c4
Michiel Sikma <michiel@sikma.org>
2015-07-17 09:20:56 +0900

    Add copyright

--- app/lib/components/ClickMe/ClickMeStore.js
+++ app/lib/components/ClickMe/ClickMeStore.js
@@ -1,3 +1,7 @@
+// React Hot Boilerplate (replace this copyright notice with your own)
+// (C) 2015, Michiel Sikma <michiel@sikma.org>
+// MIT License
+
 import Alt from 'etc/Alt';
 import ClickMeActions from './ClickMeActions';
 
